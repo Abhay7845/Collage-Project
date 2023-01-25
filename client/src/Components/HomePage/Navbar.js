165dd347208bfe152308f1a048670077fb874c9d
Abhay Kumar <abhaykr95678@gmail.com>
2023-01-25 18:25:56 +0530

    login username

--- client/src/Components/HomePage/Navbar.js
+++ client/src/Components/HomePage/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import * as Icon from "react-bootstrap-icons";
-import { FaUser } from "react-icons/fa";
 import image from "../../Asset/img/a_logo.png";
 
 import "../Style/HomePage.css";
@@ -41,19 +40,8 @@ const Navbar = () => {
         }`}
       >
         <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-lg-0">
               {!localStorage.getItem("token") ? (
                 <li className="nav-item">
                   <Link className="navbar-brand" to="/home">
@@ -106,13 +94,13 @@ const Navbar = () => {
                 to="/translate"
               >
                 <Icon.Translate
-                  className="my-1 mx-1"
+                  className="my-1 mx-2"
                   size={22}
                   cursor="pointer"
                 />
               </Link>
               <Icon.SunFill
-                className="my-1 lightDark mx-1"
+                className="my-1 lightDark mx-2"
                 size={22}
                 cursor="pointer"
                 onClick={ChangeTheme}
@@ -135,23 +123,9 @@ const Navbar = () => {
                     }`}
                     to="/profile"
                   >
-                    <FaUser
-                      size={20}
-                      color={`${
-                        location.pathname === "/profile" ? "black" : "white"
-                      }`}
-                      className="mx-4"
-                    />
-                    <p
-                      style={{ fontSize: "10px" }}
-                      color={`${
-                        location.pathname === "/profile" ? "black" : "white"
-                      }`}
-                    >
-                      {userInfo.name}
-                    </p>
+                    {userInfo.name}
                   </Link>
-                  <b className="logoutBtn" onClick={LogOut}>
+                  <b className="logoutBtn mx-3" onClick={LogOut}>
                     LOGOUT
                   </b>
                 </>
