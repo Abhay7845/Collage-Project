0e2f3d70d38e425f68ece97bd2c3387737e9685b
abhay kumar <abhaykr95678@gmail.com>
2023-07-19 22:23:45 +0530

    updated

--- client/src/Components/User/Register.js
+++ client/src/Components/User/Register.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+/** @format */
+
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
 import "../Style/RegisterLogin.css";
@@ -14,14 +16,23 @@ import {
 import { HOST_URL } from "../../API/Host";
 
 const Register = (props) => {
+  const { showAlert } = props;
   const [loading, setLoading] = useState(false);
   const [passwordShown, setPasswordShown] = useState(false);
   const navigate = useNavigate();
+  const LoginTime = localStorage.getItem("loginTime");
 
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
   };
-
+  useEffect(() => {
+    const currentTime = new Date().getTime();
+    if (currentTime < LoginTime) {
+      navigate("/theAryan/group/home");
+    } else {
+      navigate("/theAryan/group/login");
+    }
+  }, [LoginTime, navigate]);
   const onSubmit = async (payload) => {
     setLoading(true);
     const { name, email, phone, password } = payload;
@@ -34,13 +45,15 @@ const Register = (props) => {
     });
     result = await result.json();
     if (result.success) {
+      const loginTime = new Date(result.loginTime).getTime();
       localStorage.setItem("token", result.token);
-      props.showAlert("Your Account created Successfully", "success");
+      localStorage.setItem("loginTime", loginTime);
+      showAlert("Your Account created Successfully", "success");
       navigate("/theAryan/group/home");
       setLoading(false);
     }
     if (result.success === false) {
-      props.showAlert("Email is Already Registered", "danger");
+      showAlert("Email is Already Registered", "danger");
       setLoading(false);
     }
     if (result.errors) {
@@ -50,67 +63,66 @@ const Register = (props) => {
   };
 
   return (
-    <>
-      <div className="row mx-0">
-        <div className="col RegisterLeftStyle">
-          <h5 className="text-center my-5 text-info">REGISTER WITH US</h5>
+    <div>
+      <div className='row mx-0'>
+        <div className='col RegisterLeftStyle'>
+          <h5 className='text-center my-5 text-info'>REGISTER WITH US</h5>
         </div>
-        <div className="col RegisterLeftRight">
-          <div className="text-center text-info my-4">
+        <div className='col RegisterLeftRight'>
+          <div className='text-center text-info my-4'>
             <FaUserAlt size={30} />
             <h4>REGISTER</h4>
           </div>
           <Formik
             initialValues={RegisterInitialValue}
             validationSchema={RegisterSchema}
-            onSubmit={(payload) => onSubmit(payload)}
-          >
+            onSubmit={(payload) => onSubmit(payload)}>
             <Form>
               <div>
                 <b>
-                  Full Name<span className="text-danger"> *</span>
+                  Full Name<span className='text-danger'> *</span>
                 </b>
-                <Field placeholder="Full NAME" name="name" className="GInput" />
-                <ShowError name="name" />
+                <Field placeholder='Full NAME' name='name' className='GInput' />
+                <ShowError name='name' />
               </div>
-              <div className="my-2">
+              <div className='my-2'>
                 <b>
-                  Email Address <span className="text-danger"> *</span>
+                  Email Address <span className='text-danger'> *</span>
                 </b>
                 <Field
-                  placeholder="Email Address"
-                  name="email"
-                  className="GInput"
+                  placeholder='Email Address'
+                  name='email'
+                  className='GInput'
                 />
-                <ShowError name="email" />
+                <ShowError name='email' />
               </div>
-              <div className="my-2">
+              <div className='my-2'>
                 <b>
-                  Phone Number<span className="text-danger"> *</span>
+                  Phone Number<span className='text-danger'> *</span>
                 </b>
                 <Field
-                  placeholder="Phone Number"
-                  name="phone"
-                  className="GInput"
+                  placeholder='Phone Number'
+                  name='phone'
+                  className='GInput'
                 />
-                <ShowError name="phone" />
+                <ShowError name='phone' />
               </div>
-              <div className="my-2">
+              <div className='my-2'>
                 <b>
-                  Set Password <span className="text-danger"> *</span>
+                  Set Password <span className='text-danger'> *</span>
                 </b>
-                <div className="d-flex">
+                <div className='d-flex'>
                   <Field
                     type={passwordShown ? "text" : "password"}
-                    placeholder="Password"
-                    className="GInput"
-                    name="password"
+                    placeholder='Password'
+                    className='GInput'
+                    name='password'
                   />
-                  <span className="border-bottom">
+                  <span className='border-bottom'>
                     {passwordShown ? (
                       <FaRegEye
                         size={20}
-                        cursor="pointer"
+                        cursor='pointer'
                         onClick={togglePassword}
                         style={{
                           marginTop: 15,
@@ -119,25 +131,25 @@ const Register = (props) => {
                     ) : (
                       <FaRegEyeSlash
                         size={20}
-                        cursor="pointer"
+                        cursor='pointer'
                         onClick={togglePassword}
                         style={{ marginTop: 15 }}
                       />
                     )}
                   </span>
                 </div>
-                <ShowError name="password" />
+                <ShowError name='password' />
               </div>
-              <div className="d-flex justify-content-end mx-2 my-3">
-                <button type="submit" className="CButton">
+              <div className='d-flex justify-content-end mx-2 my-3'>
+                <button type='submit' className='CButton'>
                   {loading ? (
                     <span
-                      className="spinner-border spinner-border-sm"
-                      role="status"
-                      aria-hidden="true"
+                      className='spinner-border spinner-border-sm'
+                      role='status'
+                      aria-hidden='true'
                     />
                   ) : (
-                    <span className="sr-only">REGISTER</span>
+                    <span className='sr-only'>REGISTER</span>
                   )}
                 </button>
               </div>
@@ -145,10 +157,10 @@ const Register = (props) => {
           </Formik>
         </div>
       </div>
-      <div className="footerStyle">
+      <div className='footerStyle'>
         <Footer />
       </div>
-    </>
+    </div>
   );
 };
 
