ed98a4c4a5d9fa68178cb417071d18a9c54e63f2
abhay kumar <abhaykr95678@gmail.com>
2023-08-09 23:05:41 +0530

    updated

--- client/src/Components/HomePage/Navbar.js
+++ client/src/Components/HomePage/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = () => {
   return (
     <div>
       <nav
-        className={`navbar navbar-expand-lg ${
+        className={`navbar navbar-expand-lg sticky-top ${
           theme === "light-theme" ? "bg-info" : "bg-primary"
         }`}
       >
