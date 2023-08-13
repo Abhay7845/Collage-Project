eb311bfb4f59a99f6013dfa3c2ec6e33557fcbf2
abhay kumar <abhaykr95678@gmail.com>
2023-08-13 09:40:28 +0530

    updated

--- client/src/Redux/APICall/ProfileAPI.js
+++ client/src/Redux/APICall/ProfileAPI.js
@@ -3,12 +3,14 @@ import { HOST_URL } from "../../API/Host";
 
 export const ProfileAPI = async () => {
   const userAccessToken = localStorage.getItem("token");
-  const response = await axios.get(`${HOST_URL}/fetchUser`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: userAccessToken,
-    },
-  });
+  const response = await axios
+    .get(`${HOST_URL}/fetchUser`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: userAccessToken,
+      },
+    })
+    .catch((error) => console.log(""));
   const data = response;
   return data;
 };
